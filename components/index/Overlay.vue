19edf74a68fea6b4dcf24385770e537a0cb2fd55
Papakkkarlo <egoriv1802@gmail.com>
2020-03-18 13:45:41 +0300

    change course structure

--- components/index/Overlay.vue
+++ components/index/Overlay.vue
@@ -13,14 +13,13 @@
                                     <Scroller :game="g"/>
                                 </div>
                                 <div class="desc">
+                                    <div class="desc-head">{{g.project}}</div>
+                                    <div class="desc-body">{{ g.semestr }}</div>
                                     <div class="desc-head">Авторы</div>
-                                    <div class="desc-body">
-                                        <div class="course">{{ g.course }}</div>
-                                        <div>{{ g.authors.join() }}</div>
-                                    </div>
-                                    <div class="desc-body"></div>
+                                    <div class="desc-body">{{ g.authors.join() }}</div>
                                     <div class="desc-head">Описание</div>
                                     <div class="desc-body">{{ g.description_ru }}</div>
+                                    <a v-if="g.addition" class="addition" :href="g.addition">Подробнее об игре</a>
                                 </div>
                             </div>
                         </div>
@@ -55,7 +54,8 @@ export default{
                     description: '',
                     description_ru: 'Содержать бар на собственном астероиде — это далеко не сказка. То залетают кучи космического мусора, то орды злобных роботов пытаются украсть ваш честный заработок.',
                     video: 'vkd-wUrAHck',
-                    course: '2 семестр "Инструментальные средства разработки видеоигр" Курсовой проект',
+                    project: 'Курсовой проект',
+                    semestr: '2 семестр "Инструментальные средства разработки видеоигр"',
                     authors: ['Симонов Андрей', 'Александр Гутриц', 'Перепелова Анастасия', 'Роман Соловьев'],
                     addition: '',
                     screens: [
@@ -69,7 +69,8 @@ export default{
                     description: 'A potion-throwing wingsuit-flying adventure game',
                     description_ru: 'Адвенчура про варку зелий и полёты на фентезийном вингсьюте',
                     video: 'eddgu9prVO0',
-                    course: '2 семестр "Инструментальные средства разработки видеоигр" Курсовой проект',
+                    project: 'Курсовой проект',
+                    semestr: '2 семестр "Инструментальные средства разработки видеоигр"',
                     authors: ['Волкова Наталья Алексеевна', 'Дудаков Леонид Сергеевич', 'Тихоненко Никита Александрович', 'Тихоненко Никита Александрович'],
                     addition: 'https://twitter.com/Alchemica_Game',
                     screens: [
@@ -83,7 +84,8 @@ export default{
                     description: 'Greedy Gramps is action role-playing game with competitive elements. The game combines snares building stage inside gramp’s house and dungeon crawling mechanics. Each player places traps to defend their own house from other players and then attack enemy house to find the bequest and earn most points on current map.',
                     description_ru: 'Greedy Gramps -соревновательная ролевая игра. Игра сочетает в себе механики строительства ловушек в доме Грэмпа и исследования подземелий. Каждый из игроков ставит ловушки, чтобы защитить свой дом от других игроков, а затем атакует вражеский дом, чтобы найти наследство и заработать очки.',
                     video: 'V0gtwicfIrA',
-                    course: '2 семестр "Инструментальные средства разработки видеоигр" Курсовой проект',
+                    project: 'Курсовой проект',
+                    semestr: '2 семестр "Инструментальные средства разработки видеоигр"',
                     authors: ['Горшков Владислав Вадимович', 'Леонов Владислав Романович', 'Муратов Тимур Ринатович', 'Нурлыгаянов Никита Рамильевич'],
                     addition: '',
                     screens: [
@@ -96,7 +98,8 @@ export default{
                     description: "You play as a small and brave vacuum cleaner robot who wants to clean the whole house before its owners' arrival. But of course the stinky pets hate the newcomer and get in its way all the time. Use the help of other electrical devices connected via Internet of Things to distract these noisy animals. Don't let them catch you!",
                     description_ru: "Вы играете за маленького и храброго робота-пылесоса, который хочет убрать дом до прихода его хозяев. Но это не нравится домашним животным и они постоянно пытаются вам помешать. Отвлеките животных с помощью устройств, подключенных через Интернет вещей, не дайте им поймать вас!",
                     video: 'S7JNxA1PgKk',
-                    course: 'Личный независимый проект - третий семестр',
+                    project: 'Личный независимый проект',
+                    semestr: 'Третий семестр',
                     authors: ['Чеботков Даниил'],
                     addition: 'https://stockyspark.itch.io/smart-ass?fbclid=IwAR1_fRcPcK1rNWP65usHzywaaSC_0KQ3Xi0O6HTDVTtvoP-0wbCmWxeYKOE',
                     screens: [
@@ -109,7 +112,8 @@ export default{
                     description: "Race Trace is a humorous chair race game prototype where you play as one of office workers. It is friday night and everyone dreams to finally get to the bar and drink a few bottles of beer. Hurry up! The first one at the bar gets a six-pack for free!",
                     description_ru: "Race-Trace — это гонки на офисных стульях, где вы играете за одного из сотрудников. В пятницу вечером все мечтают наконец-то добраться до бара и выпить пару бутылок пива. Поторопись, первый получает шесть бутылок бесплатно!",
                     video: 'LL67854Y1yY',
-                    course: 'Личный независимый проект - третий семестр',
+                    project: 'Личный независимый проект',
+                    semestr: 'Третий семестр',
                     authors: ['Чеботков Даниил'],
                     addition: 'https://stockyspark.itch.io/race-trace?fbclid=IwAR1KIJ47iOvCJumwF-ZHPXkyiUSHyQwkeAbexcUhZ-f1KNvAzWp0avvXf3Y',
                     screens: [
@@ -123,7 +127,8 @@ export default{
                     description: "After an accident your soul was broken and you was closed inside your head.Open all gates to leave this place.Use special abilities to pass the obstacles.New ability for every level.",
                     description_ru: "После несчастного случая ваша душа разбита, и вы оказались заперты в своей голове. Откройте все ворота, чтобы выбраться.Используйте свои особые способности, чтобы преодолевать препятствия. Новые способности для каждого уровня.",
                     video: '6_fbjVN307E',
-                    course: '1 семестр Курсовой проект - "Введение в игровую разработку"',
+                    project: 'Курсовой проект',
+                    semestr: '1 семестр "Введение в игровую разработку"',
                     authors: ['Тимур Сайфуллин'],
                     addition: '',
                     screens: [
@@ -141,8 +146,9 @@ export default{
                     name: 'Equilibrium',
                     description: "Арена-шутер в стилистике retrowave, в котором игроку предстоит защищать свой кристалл от врагов и уничтожать их кристаллы",
                     description_ru: "Арена-шутер в стилистике retrowave, в котором игроку предстоит защищать свой кристалл от врагов и уничтожать их кристаллы",
-                    video: '',
-                    course: '1 семестр Курсовой проект - "Введение в игровую разработку"',
+                    video: '2uhJ-396fBI',
+                    project: 'Курсовой проект',
+                    semestr: '1 семестр "Введение в игровую разработку"',
                     authors: ['Черняков Никита'],
                     addition: '',
                     screens: [
