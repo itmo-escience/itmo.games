5ec11586dbee7cd98fbf02162de4025460d53569
Papakkkarlo <egoriv1802@gmail.com>
2020-03-18 11:48:10 +0300

    Student works refactor

--- components/index/StudentsWorks.vue
+++ components/index/StudentsWorks.vue
@@ -5,51 +5,15 @@
                 <article class="labs">
                     <h1>Работы студентов</h1>
                     <div class="labs-slider">
-                        <swiper class="swiper" :options="swiperOption">
-                            <swiper-slide>
-                                <div class="lab" @click="openGame('SUPERMASSIVE BLACK HOLE')">
-                                    <img src="/labs/prew_main_page/SUPERMASSIVE-BLACK-HOLE.png"/>
-                                    <div class="lab-title">SUPERMASSIVE BLACK HOLE</div>
-                                </div>
-                            </swiper-slide>
-                            <swiper-slide>
-                                <div class="lab" @click="openGame('Alchemica')">
-                                    <img src="/labs/prew_main_page/Alchemica.gif"/>
-                                    <div class="lab-title">Alchemica</div>
-                                </div>
-                            </swiper-slide>
-                            <swiper-slide>
-                                <div class="lab" @click="openGame('Greedy Gramps')">
-                                    <img src="/labs/prew_main_page/Greedy-Gramps.png"/>
-                                    <div class="lab-title">Greedy Gramps</div>
-                                </div>
-                            </swiper-slide>
-                            <swiper-slide>
-                                <div class="lab" @click="openGame('Smart Ass')">
-                                    <img src="/labs/prew_main_page/Smart-Ass.png"/>
-                                    <div class="lab-title">Smart Ass</div>
-                                </div>
-                            </swiper-slide>
-                            <swiper-slide>
-                                <div class="lab" @click="openGame('Race-Trace')">
-                                    <img src="/labs/prew_main_page/Race-Trace.png"/>
-                                    <div class="lab-title">Race Trace</div>
-                                </div>
-                            </swiper-slide>
-                            <swiper-slide>
-                                <div class="lab" @click="openGame('The Gate')">
-                                    <img src="/labs/prew_main_page/The-Gate.png"/>
-                                    <div class="lab-title">The Gate</div>
-                                </div>
-                            </swiper-slide>
-                            <swiper-slide>
-                                <div class="lab" @click="openGame('Equilibrium')">
-                                    <img src="/labs/prew_main_page/Equilibrium.png"/>
-                                    <div class="lab-title">Equilibrium</div>
-                                </div>
-                            </swiper-slide>
-                            <div class="swiper-scrollbar" slot="scrollbar"></div>
-                        </swiper>
+                    <swiper class="swiper" :options="swiperOption">
+                        <swiper-slide  v-for="g in games" :key="g.name">
+                            <div class="lab" @click="openGame(g.name)">
+                                <img class="slider-img" :src="g.img"/>
+                                <div class="lab-title">{{g.name}}</div>
+                            </div>
+                        </swiper-slide>
+                        <div class="swiper-scrollbar" slot="scrollbar"></div>
+                    </swiper>
                     </div>
                 </article>
             </div>
@@ -68,12 +32,42 @@ import Overlay from '~/components/index/Overlay.vue'
         game: '',
         swiperOption: {
           slidesPerView: 4,
-          spaceBetween: 30,
+          spaceBetween: 90,
           scrollbar: {
             el: '.swiper-scrollbar',
             hide: false
           }
-        }
+        },
+        games:[
+            {
+                name: 'SUPERMASSIVE BLACK HOLE',
+                img: '/labs/prew_main_page/SUPERMASSIVE-BLACK-HOLE.png'
+            },
+            {
+                name: 'Alchemica',
+                img: '/labs/prew_main_page/Alchemica.gif'
+            },
+            {
+                name: 'Greedy Gramps',
+                img: '/labs/prew_main_page/Greedy-Gramps.png'
+            },
+            {
+                name: 'Smart Ass',
+                img: '/labs/prew_main_page/Smart-Ass.png'
+            },
+            {
+                name: 'Race-Trace',
+                img: '/labs/prew_main_page/Race-Trace.png'
+            },
+            {
+                name: 'The Gate',
+                img: '/labs/prew_main_page/The-Gate.png'
+            },
+            {
+                name: 'Equilibrium',
+                img: '/labs/prew_main_page/Equilibrium.png'
+            }
+        ]
       }
     },
     methods:{
@@ -91,7 +85,6 @@ import Overlay from '~/components/index/Overlay.vue'
     .labs {
         width: 100%;
         margin-top: 200px;
-        padding: 0 80px;
 
         h1 {
             text-align: center;
@@ -107,7 +100,7 @@ import Overlay from '~/components/index/Overlay.vue'
     }
     .labs-slider {
         position: relative;
-        width: 670px;
+        width: 100%;
         margin: 90px auto 160px auto;
     }
     .lab-title{
@@ -115,5 +108,8 @@ import Overlay from '~/components/index/Overlay.vue'
     }
     .swiper-scrollbar{
         background: #323947;
+    }
+    .slider-img{
+        max-width: inherit;
     }
 </style>
